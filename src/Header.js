f61394b4fadc3d46abe7b5d824741429a8e32a76
Burak Guney <burakguney.computer@gmail.com>
2023-05-15 17:55:38 +0300

    userContext added

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,52 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { UserContext } from './UserContext'
 
 const Header = () => {
+
+    const { userInfo, setUserInfo } = useContext(UserContext)
+
+    useEffect(() => {
+        fetch('http://localhost:8080/profile', {
+            credentials: 'include'
+        }).then(response => {
+            response.json().then(userInfo => {
+                setUserInfo(userInfo)
+            })
+        })
+    }, [setUserInfo])
+
+    const logout = () => {
+        fetch('http://localhost:8080/logout', {
+            credentials: 'include',
+            method: 'POST'
+        })
+
+        setUserInfo('')
+    }
+
+    const username = userInfo?.username
+
     return (
         <header>
             <Link to='/' className='logo'>MyBlog</Link>
             <nav>
-                <Link to='/login'>Login</Link>
-                <Link to='/register'>Register</Link>
+                {
+                    username && (
+                        <>
+                            <Link to='/create'>Create new post</Link>
+                            <Link onClick={logout}>Logout</Link>
+                        </>
+                    )
+                }
+                {
+                    !username && (
+                        <>
+                            <Link to='/login'>Login</Link>
+                            <Link to='/register'>Register</Link>
+                        </>
+                    )
+                }
             </nav>
         </header>
     )
