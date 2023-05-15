bcdb3f04db6e6efcc374577588c81b40a334c2e7
Burak Guney <burakguney.computer@gmail.com>
2023-05-15 15:38:45 +0300

    new feautures added

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,12 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const RegisterPage = () => {
+
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    async function register(event) {
+        event.preventDefault()
+
+        try {
+            const response = await fetch('http://localhost:8080/register', {
+                method: 'POST',
+                body: JSON.stringify({ username, password }),
+                headers: { 'Content-Type': 'application/json' },
+            })
+
+            if (response.status === 201) {
+                alert('registration successful')
+            } else {
+                alert('registration failed')
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <div>
-            <form action='' className='register'>
+            <form className='register' onSubmit={register}>
                 <h1>Register</h1>
-                <input type='text' placeholder='username'></input>
-                <input type='password' placeholder='password'></input>
+                <input
+                    type='text'
+                    placeholder='username'
+                    value={username}
+                    onChange={event => setUsername(event.target.value)}>
+                </input>
+                <input
+                    type='password'
+                    placeholder='password'
+                    value={password}
+                    onChange={event => setPassword(event.target.value)}>
+                </input>
                 <button>Register</button>
             </form>
         </div>
