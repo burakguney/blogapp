a8505ba846030a9526eedb6b6d72c63b9819e76f
Burak Guney <burakguney.computer@gmail.com>
2023-05-18 21:27:33 +0300

    single post

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,3 +1,4 @@
+import { formatISO9075 } from 'date-fns'
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
@@ -11,18 +12,20 @@ const PostPage = () => {
                 setPostInfo(postInfo)
             })
         })
-    }, [])
+    }, [id])
 
     if (!postInfo) return ''
 
     return (
         <div>
             <div className='post-page'>
+                <h1>{postInfo.title}</h1>
+                <time>{formatISO9075(new Date(postInfo.createdAt))}</time>
+                <div className='author'>by {postInfo.author.username}</div>
                 <div className='image'>
                     <img src={`http://localhost:8080/${postInfo.cover}`} alt="" />
                 </div>
-                <h1>{postInfo.title}</h1>
-                <div></div>
+                <div className='content' dangerouslySetInnerHTML={{ __html: postInfo.content }}></div>
             </div>
         </div>
     )
